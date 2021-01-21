d6b147729de0430aebe36e33036571bd35f9d106
aurelien-barbotin <aurelien.barbotin@dtc.ox.ac.uk>
2021-01-21 14:25:37 +0100

    Change name of Thorlabs wrapper and code cleanup

--- microscope/_wrappers/thorlabs_motionControl.py
+++ microscope/_wrappers/thorlabs_motionControl.py
@@ -8,11 +8,10 @@ Wrapper to the Kinesis (thorlabs) C API for NanoMax stage
 """
 
 import ctypes
-import glob
 
-from ctypes import c_char, c_char_p, c_double, c_int, c_uint, c_uint32, c_short, c_bool, c_long, POINTER
-from ctypes.wintypes import DWORD, WORD
-import time
+from ctypes import c_char_p, c_int, c_short, c_bool
+from ctypes.wintypes import DWORD
+
 
 
 #path = "Thorlabs.MotionControl.Benchtop.Piezo.dll"
@@ -48,9 +47,6 @@ SBC_Home = make_prototype("SBC_Home",[c_char_p,c_short],restype = c_short)
 
 SBC_MoveToPosition = make_prototype("SBC_MoveToPosition",[c_char_p,c_short, c_int], restype = c_short)
 # [serialnr, channel, index]
-
-# Not sure it works
-SBC_WaitForMessage = make_prototype("SBC_WaitForMessage",[c_char_p, c_short, POINTER(WORD), POINTER(WORD), POINTER(DWORD)], restype=c_bool)
 
 SBC_StopPolling = make_prototype("SBC_StopPolling",[c_char_p, c_short])
 # [serialNo, channel]
