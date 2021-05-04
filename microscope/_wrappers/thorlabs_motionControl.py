ba59a374f1b08d16b8ba009f345739b92316eb2b
David Miguel Susano Pinto <david.pinto@bioch.ox.ac.uk>
2021-05-04 13:12:07 +0100

    blacken code

--- microscope/_wrappers/thorlabs_motionControl.py
+++ microscope/_wrappers/thorlabs_motionControl.py
@@ -13,46 +13,61 @@ from ctypes import c_char_p, c_int, c_short, c_bool, POINTER, c_double
 from ctypes.wintypes import DWORD
 
 
-
-#path = "Thorlabs.MotionControl.Benchtop.Piezo.dll"
+# path = "Thorlabs.MotionControl.Benchtop.Piezo.dll"
 SDK = ctypes.WinDLL("Thorlabs.MotionControl.Benchtop.StepperMotor.dll")
 # SDK = ctypes.CDLL(path)
 
 # from BMC stuff
 RC = c_short  # enum for error codes
+
+
 def make_prototype(name, argtypes, restype=RC):
     func = getattr(SDK, name)
     func.argtypes = argtypes
     func.restype = restype
     return func
 
-TLI_BuildDeviceList = make_prototype("TLI_BuildDeviceList",[],restype=c_short)
 
-TLI_GetDeviceListSize = make_prototype("TLI_GetDeviceListSize",[],restype=c_short)
+TLI_BuildDeviceList = make_prototype(
+    "TLI_BuildDeviceList", [], restype=c_short
+)
 
-TLI_GetDeviceListByTypeExt = make_prototype("TLI_GetDeviceListByTypeExt",[c_char_p,DWORD,c_int],restype=c_short)
+TLI_GetDeviceListSize = make_prototype(
+    "TLI_GetDeviceListSize", [], restype=c_short
+)
+
+TLI_GetDeviceListByTypeExt = make_prototype(
+    "TLI_GetDeviceListByTypeExt", [c_char_p, DWORD, c_int], restype=c_short
+)
 # [receiveBuffer, sizeOfBuffer, typeID]
 
-SBC_Open = make_prototype("SBC_Open", [c_char_p],restype = c_short)
+SBC_Open = make_prototype("SBC_Open", [c_char_p], restype=c_short)
 # [serialNo]
 
-SBC_StartPolling = make_prototype("SBC_StartPolling",[c_char_p,c_short,c_int], restype=c_bool)
+SBC_StartPolling = make_prototype(
+    "SBC_StartPolling", [c_char_p, c_short, c_int], restype=c_bool
+)
 # [serialNo, channel, milliseconds]
- 
-SBC_ClearMessageQueue = make_prototype("SBC_ClearMessageQueue",[c_char_p,c_short],restype = c_short)
+
+SBC_ClearMessageQueue = make_prototype(
+    "SBC_ClearMessageQueue", [c_char_p, c_short], restype=c_short
+)
 # [serialNo, channel]
 
-SBC_Home = make_prototype("SBC_Home",[c_char_p,c_short],restype = c_short)
+SBC_Home = make_prototype("SBC_Home", [c_char_p, c_short], restype=c_short)
 # [serialNo, channel]
 
-SBC_MoveToPosition = make_prototype("SBC_MoveToPosition",[c_char_p,c_short, c_int], restype = c_short)
+SBC_MoveToPosition = make_prototype(
+    "SBC_MoveToPosition", [c_char_p, c_short, c_int], restype=c_short
+)
 # [serialnr, channel, index]
 
-SBC_StopPolling = make_prototype("SBC_StopPolling",[c_char_p, c_short])
+SBC_StopPolling = make_prototype("SBC_StopPolling", [c_char_p, c_short])
 # [serialNo, channel]
 
-SBC_Close = make_prototype("SBC_Close",[c_char_p])
+SBC_Close = make_prototype("SBC_Close", [c_char_p])
 # [serialNo]
+
 
 def make_serialnr(sn):
     """Formatting the 8-digit int serial number (ex 70897524) for e.g SBC_Open"""
@@ -60,60 +75,68 @@ def make_serialnr(sn):
 
 
 # ----------- Untested methods ------------
-SBC_getNumChannels = make_prototype("SBC_getNumChannels", [c_char_p],restype = c_short)
+SBC_getNumChannels = make_prototype(
+    "SBC_getNumChannels", [c_char_p], restype=c_short
+)
 # [*serialNo]
 
-SBC_GetRealValueFromDeviceUnit = make_prototype("SBC_GetRealValueFromDeviceUnit", 
-                                [c_char_p, c_short, c_int, POINTER(c_double), c_int])
-#[*serialNo, channel, device_unit, *real unit, unitType]
+SBC_GetRealValueFromDeviceUnit = make_prototype(
+    "SBC_GetRealValueFromDeviceUnit",
+    [c_char_p, c_short, c_int, POINTER(c_double), c_int],
+)
+# [*serialNo, channel, device_unit, *real unit, unitType]
 
-SBC_MoveRelative =  make_prototype("SBC_MoveRelative",[c_char_p, c_short, c_int])
-#[*serialNo, channel, displacement]
+SBC_MoveRelative = make_prototype(
+    "SBC_MoveRelative", [c_char_p, c_short, c_int]
+)
+# [*serialNo, channel, displacement]
 
 SBC_GetPosition = make_prototype("SBC_GetPosition", [c_char_p, c_short], c_int)
 # [*serialNo, channel]
 
-SBC_GetMotorTravelLimits = make_prototype("SBC_GetMotorTravelLimits", 
-                      [c_char_p, c_short, POINTER(c_double), POINTER(c_double)])
+SBC_GetMotorTravelLimits = make_prototype(
+    "SBC_GetMotorTravelLimits",
+    [c_char_p, c_short, POINTER(c_double), POINTER(c_double)],
+)
 # [serial_number, channel, min_position, max_position]
 
 
 errors_dict = {
-    1:"FT_InvalidHandle - The FTDI functions have not been initialized",
-    2:"FT_DeviceNotFound - The Device could not be found",
-    3:"FT_DeviceNotOpened - The Device must be opened before it can be accessed",
-    4:"FT_IOError - An I/O Error has occured in the FTDI chip.",
-    5:"FT_InsufficientResources - There are Insufficient resources\
+    1: "FT_InvalidHandle - The FTDI functions have not been initialized",
+    2: "FT_DeviceNotFound - The Device could not be found",
+    3: "FT_DeviceNotOpened - The Device must be opened before it can be accessed",
+    4: "FT_IOError - An I/O Error has occured in the FTDI chip.",
+    5: "FT_InsufficientResources - There are Insufficient resources\
 to run this application. ",
-    6:"FT_InvalidParameter - An invalid parameter has been supplied\
+    6: "FT_InvalidParameter - An invalid parameter has been supplied\
 to the device. ",
-    7:"FT_DeviceNotPresent - The Device is no longer present ",
-    8:"FT_IncorrectDevice - The device detected does not match that expected",
-    16:"FT_NoDLLLoaded - The library for this device could not be found",
-    17:"FT_NoFunctionsAvailable - No functions available for this device",
-    18:"FT_FunctionNotAvailable - The function is not available for this device",
-    19:"FT_BadFunctionPointer - Bad function pointer detected",
-    20:"FT_GenericFunctionFail - The function failed to complete succesfully",
-    21:"FT_SpecificFunctionFail - The function failed to complete succesfully",
-    32:"TL_ALREADY_OPEN - Attempt to open a device that was already open",
-    33:"TL_NO_RESPONSE - The device has stopped responding. ",
-    34:"TL_NOT_IMPLEMENTED - This function has not been implemented. ",
-    35:"TL_FAULT_REPORTED - The device has reported a fault.",
-    36:"TL_INVALID_OPERATION - The function could not be completed at this time.",
-    41:"TL_FIRMWARE_BUG - The firmware has thrown an error ",
-    42:"TL_INITIALIZATION_FAILURE - The device has failed to initialize ",
-    43:"TL_INVALID_CHANNEL - An Invalid channel address was supplied ",
-    37:"TL_UNHOMED - The device cannot perform this function until it has been Homed. ",
-    38:"TL_INVALID_POSITION - The function cannot be performed as\
+    7: "FT_DeviceNotPresent - The Device is no longer present ",
+    8: "FT_IncorrectDevice - The device detected does not match that expected",
+    16: "FT_NoDLLLoaded - The library for this device could not be found",
+    17: "FT_NoFunctionsAvailable - No functions available for this device",
+    18: "FT_FunctionNotAvailable - The function is not available for this device",
+    19: "FT_BadFunctionPointer - Bad function pointer detected",
+    20: "FT_GenericFunctionFail - The function failed to complete succesfully",
+    21: "FT_SpecificFunctionFail - The function failed to complete succesfully",
+    32: "TL_ALREADY_OPEN - Attempt to open a device that was already open",
+    33: "TL_NO_RESPONSE - The device has stopped responding. ",
+    34: "TL_NOT_IMPLEMENTED - This function has not been implemented. ",
+    35: "TL_FAULT_REPORTED - The device has reported a fault.",
+    36: "TL_INVALID_OPERATION - The function could not be completed at this time.",
+    41: "TL_FIRMWARE_BUG - The firmware has thrown an error ",
+    42: "TL_INITIALIZATION_FAILURE - The device has failed to initialize ",
+    43: "TL_INVALID_CHANNEL - An Invalid channel address was supplied ",
+    37: "TL_UNHOMED - The device cannot perform this function until it has been Homed. ",
+    38: "TL_INVALID_POSITION - The function cannot be performed as\
 it would result in an illegal position.",
-    39:"TL_INVALID_VELOCITY_PARAMETER - An invalid velocity parameter\
+    39: "TL_INVALID_VELOCITY_PARAMETER - An invalid velocity parameter\
 was supplied. The velocity must be greater than zero. ",
-    44:"TL_CANNOT_HOME_DEVICE - This device does not support Homing\
+    44: "TL_CANNOT_HOME_DEVICE - This device does not support Homing\
 Check the Limit switch parameters are correct",
-    45:"TL_JOG_CONTINOUS_MODE - An invalid jog mode was supplied\
+    45: "TL_JOG_CONTINOUS_MODE - An invalid jog mode was supplied\
 for the jog function. ",
-    46:"TL_NO_MOTOR_INFO - There is no Motor Parameters available to convert\
+    46: "TL_NO_MOTOR_INFO - There is no Motor Parameters available to convert\
 Real World Units. ",
-    47:"TL_CMD_TEMP_UNAVAILABLE - Command temporarily unavailable, Device may\
-be busy."
+    47: "TL_CMD_TEMP_UNAVAILABLE - Command temporarily unavailable, Device may\
+be busy.",
 }
--- microscope/stages/thorlabs_nanomax.py
+++ microscope/stages/thorlabs_nanomax.py
@@ -11,10 +11,11 @@ try:
     import microscope._wrappers.thorlabs_motionControl as TMC
 except Exception as e:
     raise microscope.LibraryLoadError(e) from e
-    
+
 import microscope.abc
 
 from ctypes import c_char_p, c_double
+
 
 class ThorlabsNanoMaxAxis(microscope.abc.StageAxis):
     def __init__(self, serial_number, axis):
@@ -25,22 +26,23 @@ class ThorlabsNanoMaxAxis(microscope.abc.StageAxis):
         """
         self.serial_number = serial_number
         self.axis = axis
-        
+
     def limits(self):
         mintravel = c_double(20)
         maxtravel = c_double(10)
-        TMC.SBC_GetMotorTravelLimits(self.serial_number, self.axis, mintravel, 
-                                 maxtravel)
+        TMC.SBC_GetMotorTravelLimits(
+            self.serial_number, self.axis, mintravel, maxtravel
+        )
         return [mintravel.value, maxtravel.value]
-    
+
     def move_by(self, delta):
-        status = TMC.SBC_MoveRelative(self.serial_number,self.axis,delta)
+        status = TMC.SBC_MoveRelative(self.serial_number, self.axis, delta)
         if status:
             message = "Error"
             if status in TMC.errors_dict.keys():
                 message = TMC.errors_dict[status]
             raise microscope.DeviceError(message)
-            
+
     def move_to(self, pos):
         # pos: int
         status = TMC.SBC_MoveToPosition(self.serial_number, self.axis, pos)
@@ -49,41 +51,42 @@ class ThorlabsNanoMaxAxis(microscope.abc.StageAxis):
             if status in TMC.errors_dict.keys():
                 message = TMC.errors_dict[status]
             raise microscope.DeviceError(message)
-            
+
     def position(self):
         pos = TMC.SBC_GetPosition(self.serial_number, self.axis)
         return pos
-    
+
+
 class ThorlabsNanoMax(microscope.abc.Stage):
     """A Thorlabs Nanomax stage. So far supports only 3-axis, stepper-motor stage"""
+
     def __init__(self, serial_number: str, **kwargs) -> None:
         super().__init__(**kwargs)
-        
+
         self.serial_number = c_char_p(bytes(str(serial_number), "utf-8"))
         status = TMC.SBC_Open(self.serial_number)
 
         if status:
             raise microscope.InitialiseError(status)
         # populate axes dict
-        self.n_axes = 3 # can be updated later
-        axes_names = ["x","y","z"]
+        self.n_axes = 3  # can be updated later
+        axes_names = ["x", "y", "z"]
         self._axes = {}
         for j in range(self.n_axes):
             axn = axes_names[j]
-            self._axes[axn]=ThorlabsNanoMaxAxis(self.serial_number,j+1)
+            self._axes[axn] = ThorlabsNanoMaxAxis(self.serial_number, j + 1)
 
     def enable(self):
         # homing
         # !!! Might need to wait for homing to finish axis by axis
         for channel_nr in range(self.n_axes):
-            status = TMC.SBC_Home(self.serial_number, channel_nr+1)
+            status = TMC.SBC_Home(self.serial_number, channel_nr + 1)
             if status:
                 message = "Error"
                 if status in TMC.errors_dict.keys():
                     message = TMC.errors_dict[status]
                 raise microscope.DeviceError(message)
-        
-    
+
     def _do_shutdown(self):
         status = TMC.SBC_Close(self.serial_number)
         if status:
@@ -91,15 +94,15 @@ class ThorlabsNanoMax(microscope.abc.Stage):
             if status in TMC.errors_dict.keys():
                 message = TMC.errors_dict[status]
             raise microscope.DeviceError(message)
-        
+
     @property
     def axes(self):
         return self._axes
-        
+
     def move_by(self, delta):
         for ax, delta in self.axes.items():
             self.axes[ax].move_by(delta)
-            
+
     def move_to(self, position):
         for ax, delta in self.axes.items():
             self.axes[ax].move_to(delta)
